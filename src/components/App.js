81834c95744f44c23085cfe2baf9f8fe572ec923
Casey Harding <casey.r.harding@gmail.com>
2022-06-21 12:37:10 -0400

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from 'react'
+// useState?... -> setState...hook returns an array of two values
+//                      1.) state variable
+//                      2.) function to update the state variable
+// useEffect?.. -> handle side effects -> ie things out of our control
 // 👉 STEP 2 - React Router imports (Route, Link and Switch)
 
 // Components used for the different routes
+import { Link, Route } from "react-router-dom";
+// Link sends the URL to a specified location
+// Route listens to the URL and depending on what it matches displays content
+
 import Home from './Home'
 import ItemsList from './ItemsList'
 import Item from './Item'
@@ -27,10 +35,18 @@ export default function App(props) {
         <h1 className='store-header'>Emily&apos;s Trinkets</h1>
         <div className='nav-links'>
           {/* 👉 STEP 3 - Make Links to navigate us Home (`/`) and Shop (`/items-list`) */}
+          <Link to="/">Home</Link>
+          <Link to="/items-list">Shop</Link>
         </div>
       </nav>
 
       {/* 👉 STEP 4 - Build a Switch with a Route for each of the components imported at the top */}
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/items-list">
+        <ItemsList items={stock} />
+      </Route>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -8,9 +8,18 @@ import App from './components/App'
 import './styles.less'
 
 // 👉 STEP 1 - Import React Router's Router
+import { BrowserRouter as Router } from "react-router-dom";
 
 render(
   // Wrap the <App /> in a provider
-  <App />
+  <Router>
+    <App />
+  </Router>
   , document.querySelector('#root')
 )
+
+/**
+ * match     -> contains all data about...the url
+ * location  -> contains all data about where you are
+ * history   -> contains all data about where you've been
+ */
